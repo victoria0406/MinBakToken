4550fc933fe22ec87269724479a1ee8f978845bb
Min Kim <kmin638123@gmail.com>
2023-05-19 17:50:01 +0900

    token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,39 +2,48 @@
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 // We import this library to be able to use console.log
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
 
 
 // This is the main building block for smart contracts.
-contract Token {
-    // Some string type variables to identify the token.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+contract Token is ERC721URIStorage, Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     // The fixed amount of tokens stored in an unsigned integer type variable.
     uint256 public totalSupply = 1000000;
 
     // An address type variable is used to store ethereum accounts.
-    address public owner;
+    // address public owner;
 
     // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) balances;
 
-    // The Transfer event helps off-chain aplications understand
-    // what happens within your contract.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
     /**
      * Contract initialization.
      */
-    constructor() {
-        // The totalSupply is assigned to the transaction sender, which is the
-        // account that is deploying the contract.
+    constructor() public ERC721("MyNFT", "NFT"){
         balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+        //owner = msg.sender;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256){
+    
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 
     /**
@@ -72,7 +81,7 @@ contract Token {
      * The `view` modifier indicates that it doesn't modify the contract's
      * state, which allows us to call it without executing a transaction.
      */
-    function balanceOf(address account) external view returns (uint256) {
+    function balanceOf(address account) public view override returns (uint256) {
         return balances[account];
     }
 }
