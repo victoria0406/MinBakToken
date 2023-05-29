448107dc267965216f479007677bf614d4b4a14e
Min Kim <kmin638123@gmail.com>
2023-05-29 16:56:04 +0900

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,6 @@
 const path = require("path");
 
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
-    );
-  }
-
   // ethers is available in the global scope
   const [deployer] = await ethers.getSigners();
   console.log(
